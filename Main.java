e900e97682dbdbe961adf566c511b08cab13acc6
Sunny Adhikari <sunnyadh07@gmail.com>
2021-04-04 21:41:19 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        String fruitName = "apple";
+
+            switch(fruitName) {
+                case "apple":
+                    System.out.println("Red");
+                    break;
+                case "orange":
+                    System.out.println("orange");
+                    break;
+                case "banana":
+                    System.out.println("Yelllow");
+                    break;
+                case "grape":
+                    System.out.println("green");
+                    break;
+                case "plum":
+                    System.out.println("Purple");
+                    break;
+            }
+        }
+}
+
---
+++ Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] args) {
+        int usersGoal = 120;
+        String matchGoal = usersGoal == 100 ? "1" : "0";
+        System.out.println(matchGoal);
+
+    }
+
+}
